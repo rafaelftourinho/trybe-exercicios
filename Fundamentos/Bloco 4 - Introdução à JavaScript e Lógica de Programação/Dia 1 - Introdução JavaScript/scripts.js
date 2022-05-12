b0a2d2afdec0233b245bb09c77edaec697ec73b5
Rafael França <rafaelftourinho@gmail.com>
2022-05-12 01:53:10 -0300

    Exercício variáveis

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 1 - Introdução JavaScript/scripts.js
@@ -0,0 +1,7 @@
+const myName = "Rafael França Tourinho";
+const birthCity = "Rio de Janeiro";
+birthCity = "Maceió" //aqui acontece um erro porque não se pode renomear constantes.
+let birthYear = 1990;
+birthYear = 2030;
+
+console.log(myName, birthCity, birthYear);
