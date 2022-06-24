ae592e5d23bd6dcf10489b0cfbf2f4034590b480
Rafael França <rafaelftourinho@gmail.com>
2022-06-24 12:54:17 -0300

    Exercícios Agora é prática

--- Fundamentos/Bloco 8 - High Order Functions do JavaScript ES6/Dia 2 - Higher Order Functions - forEach, find, some, every, sort/forEach/foreach.js
+++ Fundamentos/Bloco 8 - High Order Functions do JavaScript ES6/Dia 2 - Higher Order Functions - forEach, find, some, every, sort/forEach/foreach.js
@@ -1,3 +1,4 @@
+// EXEMPLO 1
 const emailListInData = [
   'roberta@email.com',
   'paulo@email.com',
@@ -11,3 +12,14 @@ const showEmailList = (email) => {
 
 emailListInData.forEach(item => showEmailList(item));
 
+
+// EXEMPLO 2
+
+const array = [1,2,3,4,5,6,7];
+
+const teste = () => {
+  const novoArray = [];
+  array.forEach(item => novoArray.push(item * 3));
+  return novoArray;
+}
+console.log(teste());
