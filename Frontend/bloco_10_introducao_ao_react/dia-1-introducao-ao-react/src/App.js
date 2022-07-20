dc54e69d1b66134b1d0876c8ea4f2be4d0daa25b
Rafael França <rafaelftourinho@gmail.com>
2022-07-19 22:35:51 -0300

    :sparkles: adicionando exercícios

--- Frontend/bloco_10_introducao_ao_react/dia-1-introducao-ao-react/src/App.js
+++ Frontend/bloco_10_introducao_ao_react/dia-1-introducao-ao-react/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import Image from './Image';
+import staringCat from './staringCat.jpg'
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <Content />
       <Footer />
+      <Image source={staringCat} alternativeText="Cute Cat starging" />
     </>
   );
 }
